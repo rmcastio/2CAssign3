2c35bb35c0dfe9cacc1ab913596682b96d9e38ca
Richard Castillo <armed_deity@hotmail.com>
2020-01-29 21:12:27 -0800

    Finished questions 2 and 3

--- script2.js
+++ script2.js
@@ -11,7 +11,7 @@
 // ...
 
 for (let i = 1; i < 100; i++)
-if (i % 15 == 0) {
+if (i % 3 == 0 && i % 5 == 0) {
     console.log("FizzBuzz");
 } else if (i % 3 == 0) {
     console.log("Fizz");
--- script3.js
+++ script3.js
@@ -11,3 +11,10 @@
 //  # # # #
 
 
+for (let i = 1; i < 9; i++) {
+    if (i % 2 == 0) {
+        console.log(" # # # #");
+    } else {
+        console.log("# # # #");
+    }
+}
